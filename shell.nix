17ab608672e58bb5513416a19e53855c3df45f46
Eva Emmerich <eva@evaemmerich.com>
2025-09-05 14:23:50 +0200

    shell.nix: add formatter, move tooling to nativeBuildInputs

--- shell.nix
+++ shell.nix
@@ -1,7 +1,10 @@
-let
-  # Rolling updates, not deterministic.
-  pkgs = import <nixpkgs> {};
+{ pkgs ? import <nixpkgs> {} }:
 
-in pkgs.mkShell {
-  buildInputs = [ pkgs.cargo pkgs.rustc ];
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [ rustc cargo gcc rustfmt clippy ];
+
+  # Certain Rust tools won't work without this
+  # This can also be fixed by using oxalica/rust-overlay and specifying the rust-src extension
+  # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela. for more details.
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 }
